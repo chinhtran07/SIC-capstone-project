e855133222736dbf141217a5898b52aeb2d9be7c
chinhtran07 <chinh.t1807@gmail.com>
2024-08-17 18:36:50 +0700

    update

--- server/src/routes/api.js
+++ server/src/routes/api.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { getThreshold, updateThreshold, setThreshold } = require('../controllers/thresholdController');
+const { getSensorData, getAllSensorDataToday } = require('../controllers/sensorDataController');
 
 router.get('/threshold', getThreshold);
 
@@ -8,5 +9,9 @@ router.get('/threshold', getThreshold);
 router.post('/threshold', setThreshold);
 
 router.put('/threshold', updateThreshold);
+
+router.get('/sensor-data/get-latest-data', getSensorData);
+
+router.get('/sensor-data/get-data-today', getAllSensorDataToday);
 
 module.exports = router;    
